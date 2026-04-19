49a91222d31ae94de2801f1e79ecf6d59bf6a0a5
kv1nk <kvink.ggs@gmail.com>
2026-04-19 23:14:53 +0300

    FirstCommit

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,27 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr[size];  // массив для хранения элементов
+  int top;      // позиция вершины стека
+ public:
+  // конструктор класса
+  TStack() : top(-1) {}
+  // проверка на пустоту
+  bool isEmpty() const { return top == -1; }
+  // проверка на заполненность
+  bool isFull() const { return top == size - 1; }
+  T get() const {
+    if (!isEmpty()) return arr[top];
+  }
+  void pop() {
+    if (!isEmpty()) top--;
+  }
+  void push(T item) {
+    if (!isFull()) arr[++top] = item;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,95 @@
 // Copyright 2025 NNTU-CS
-#include <string>
 #include <map>
+#include <string>
+
 #include "tstack.h"
 
+using namespace std;
+bool isOper(char x) {
+  if (x > 41 && x < 48) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+bool isNum(char x) {
+  if (x >= ('0' + 0) && x <= ('9' + 0)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int CharToNum(char x) { return x - '0'; }
+
+int getPrior(char ch) {
+  if (ch == '+' || ch == '-') return 1;
+  if (ch == '*' || ch == '/') return 2;
+  return 0;
+}
+
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+  TStack<char, 10> stack;
+  string res = "";
+
+  for (int i = 0; i < inf.length(); i++) {
+    char ch = inf[i];
+
+    if (isNum(ch)) {
+      if (res != "") res += ' ';
+      res += ch;
+    } else if (ch == '(') {
+      stack.push(ch);
+    } else if (ch == ')') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        res += ' ';
+        res += stack.get();
+        stack.pop();
+      }
+      if (!stack.isEmpty()) stack.pop();
+    } else if (isOper(ch)) {
+      while (!stack.isEmpty() && stack.get() != '(' &&
+             getPrior(stack.get()) >= getPrior(ch)) {
+        res += ' ';
+        res += stack.get();
+        stack.pop();
+      }
+      stack.push(ch);
+    }
+  }
+  while (!stack.isEmpty()) {
+    res += ' ';
+    res += stack.get();
+    stack.pop();
+  }
+  return res;
 }
 
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 10> stack;
+  for (int i = 0; i <= pref.length(); i++) {
+    char ch = pref[i];
+
+    if (ch == ' ') continue;
+
+    if (isNum(ch)) {
+      stack.push(CharToNum(ch));
+    } else if (isOper(ch)) {
+      int k = stack.get();
+      stack.pop();
+      int j = stack.get();
+      stack.pop();
+
+      int res = 0;
+
+      if (ch == '+') res = k + j;
+      if (ch == '-') res = j - k;
+      if (ch == '/') res = j / k;
+      if (ch == '*') res = j * k;
+
+      stack.push(res);
+    }
+  }
+  return stack.get();
 }
